5c13efa5aa7ac1616d7ba691506153eeeec8bdf8
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-30 22:45:19 +0200

    Update main.tf

--- k8s-cluster-terraform/main.tf
+++ k8s-cluster-terraform/main.tf
@@ -1,1 +1,32 @@
+resource "aws_instance" "kube_control_plane" {
+  ami             = "ami-xxxxxxxx"  # Remplace par l'AMI Ubuntu correcte
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  private_ip      = "192.168.56.10"
+  
+  tags = {
+    Name = "kube-control-plane"
+  }
+}
 
+resource "aws_instance" "kube_node1" {
+  ami             = "ami-xxxxxxxx"
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  private_ip      = "192.168.56.11"
+
+  tags = {
+    Name = "kube-node1"
+  }
+}
+
+resource "aws_instance" "kube_node2" {
+  ami             = "ami-xxxxxxxx"
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  private_ip      = "192.168.56.12"
+
+  tags = {
+    Name = "kube-node2"
+  }
+}
