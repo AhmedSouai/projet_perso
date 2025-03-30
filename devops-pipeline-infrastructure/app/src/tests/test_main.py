703589abad3c1a9258005a7353c9578d23ef98b0
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-31 00:39:46 +0200

    Update test_main.py

--- devops-pipeline-infrastructure/app/src/tests/test_main.py
+++ devops-pipeline-infrastructure/app/src/tests/test_main.py
@@ -1,1 +1,19 @@
+# app/src/tests/test_main.py
+import unittest
+from main import app
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data, b"Hello, World!")
+
+    def test_multiply(self):
+        response = self.app.get('/multiply/2/3')
+        self.assertEqual(response.data, b"6")
+
+if __name__ == '__main__':
+    unittest.main()
 
