ac9e8a55c92cb0d55a17abbc41c964b94052abcb
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-31 00:39:59 +0200

    Update test_integration.py

--- devops-pipeline-infrastructure/app/src/tests/test_integration.py
+++ devops-pipeline-infrastructure/app/src/tests/test_integration.py
@@ -1,1 +1,15 @@
+# app/src/tests/test_integration.py
+import unittest
+from main import app
+
+class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_multiply_integration(self):
+        response = self.app.get('/multiply/4/5')
+        self.assertEqual(response.data, b"20")
+
+if __name__ == '__main__':
+    unittest.main()
 
