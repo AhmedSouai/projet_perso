0fc0c0103fd751f5ffd75021e6e4a1260ba8eb42
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-31 00:38:49 +0200

    Update main.py

--- devops-pipeline-infrastructure/app/src/main.py
+++ devops-pipeline-infrastructure/app/src/main.py
@@ -1,1 +1,16 @@
+# app/src/main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+@app.route('/multiply/<int:a>/<int:b>')
+def multiply(a, b):
+    return str(a * b)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
 
