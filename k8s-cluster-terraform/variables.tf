72f5c17614512265ff1eba1923c7cf4c2d8c8836
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-30 22:33:30 +0200

    Update variables.tf

--- k8s-cluster-terraform/variables.tf
+++ k8s-cluster-terraform/variables.tf
@@ -1,1 +1,16 @@
+variable "aws_access_key" {
+  description = "Clé d'accès AWS"
+  type        = string
+  sensitive   = true
+}
 
+variable "aws_secret_access_key" {
+  description = "Clé secrète AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "instance_type" {
+  description = "Type de l'instance EC2"
+  default     = "t2.micro"
+}
