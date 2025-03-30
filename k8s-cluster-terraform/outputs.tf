e5f5dc743eeea70f8f0f26a1d44855c3738e2329
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2025-03-30 22:37:19 +0200

    Update outputs.tf

--- k8s-cluster-terraform/outputs.tf
+++ k8s-cluster-terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "kube_control_plane_ip" {
+  value = aws_instance.kube_control_plane.private_ip
+}
+
+output "kube_node1_ip" {
+  value = aws_instance.kube_node1.private_ip
+}
+
+output "kube_node2_ip" {
+  value = aws_instance.kube_node2.private_ip
+}
 
